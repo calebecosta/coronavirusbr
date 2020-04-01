728bcf7a5c06749e113ec6f426d7c11ab174bb2f
Calebe <calebekosta@gmail.com>
2020-04-01 15:51:27 -0300

    commit

---
+++ app.js
@@ -0,0 +1,36 @@
+let express = require("express");
+let app = express();
+let cors = require("cors");
+const axios = require('axios').default;
+let gov = require('./gov');
+
+app.use(cors());
+
+var port = process.env.PORT || 3000;
+app.listen(port,"0.0.0.0",function(){
+    console.log("rodando")
+})
+
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
+app.use(bodyParser.json())
+
+app.get('/cases', (req, res) => {
+gov.gov(function(callback){
+    res.send(callback);
+var dados = JSON.parse(callback);
+
+console.log(dados.results[0].total_confirmado);
+   res.end();
+})
+  
+})
+
+
+
+
+
---
+++ gov.js
@@ -0,0 +1,28 @@
+const axios = require('axios').default;
+let request = require ('request');
+
+exports.gov = function(callback){
+    request({
+        headers: {
+            'x-parse-application-id' : 'unAFkcaNDeXajurGB7LChj8SgQYS2ptm',
+           'Content-Type': 'application/json' ,
+        },
+    uri: "https://xx9p7hp1p7.execute-api.us-east-1.amazonaws.com/prod/PortalGeral/",
+    method: "GET",
+}, function(err, body) {
+    if (body.statusCode == 200) {
+
+callback(body.body);
+
+    }else{  
+      callback({"error": err});
+    }
+})
+};
+
+
+
+
+
+
+
